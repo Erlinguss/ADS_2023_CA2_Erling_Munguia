6f265c29aa20e8b3832cb2db657fa4797f0cebcc
Erlinguss <D00251914@student.dkit.ie>
2023-11-06 14:14:35 +0000

    Start Readimng the External file

---
+++ ADS_2023_CA2_Erling_Munguia/XMLParser.cpp
@@ -0,0 +1,10 @@
+#include "XMLParser.h"
+
+
+int main() {
+    const std::string xmlFileName = "C:/Users/User/source/repos/ADS_2023_CA2_Erling_Munguia/ADS_2023_CA2_Erling_Munguia/Example.xml";
+    XMLParser xmlParser(xmlFileName); 
+    xmlParser.parse(); 
+   
+    return 0;
+}
---
+++ ADS_2023_CA2_Erling_Munguia/XMLParser.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class XMLParser
+{
+public:
+    XMLParser(const string& xmlFileName); 
+    void parse(); 
+private:
+    string xmlFileName;
+};
+
+
+
+XMLParser::XMLParser(const string& xmlFileName)
+{
+
+}
+void XMLParser::parse()
+{
+
+}
