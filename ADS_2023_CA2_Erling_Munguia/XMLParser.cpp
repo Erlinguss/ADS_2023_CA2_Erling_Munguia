f483993526e760ba099ffab9dca4ba5e88607f41
Erlinguss <D00251914@student.dkit.ie>
2023-11-13 14:33:47 +0000

    Adding Dlist.h, DlistIterator.h, DListNode.h, Tree.h, TreeIterator.h classes.

--- ADS_2023_CA2_Erling_Munguia/XMLParser.cpp
+++ ADS_2023_CA2_Erling_Munguia/XMLParser.cpp
@@ -1,5 +1,5 @@
 #include "XMLParser.h"
-
+#include "Tree.h"
 
 int main() {
     const std::string xmlFileName = "C:/Users/User/source/repos/ADS_2023_CA2_Erling_Munguia/ADS_2023_CA2_Erling_Munguia/Example.xml";
