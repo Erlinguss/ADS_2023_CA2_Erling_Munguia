32039ce07f62ec75d621ea0df246655932bde319
Erlinguss <D00251914@student.dkit.ie>
2023-11-22 00:03:34 +0000

    Built XMLTree and PrintTree Function.

--- ADS_2023_CA2_Erling_Munguia/XMLParser.cpp
+++ ADS_2023_CA2_Erling_Munguia/XMLParser.cpp
@@ -9,7 +9,25 @@ using namespace std;
 #include "SFML/Graphics.hpp"
 
 
-// === Implementation of XMLParser ===
+// ===== Function to print the tree structure =====
+void printTree(Tree<string>* node, int level = 0) {
+    if (node == nullptr) {
+        return;
+    }
+
+    for (int i = 0; i < level; ++i) {
+        cout << "  ";
+    }
+    cout << node->data << endl;
+
+    DListIterator<Tree<string>*> childIter = node->children->getIterator();
+    while (childIter.isValid()) {
+        printTree(childIter.item(), level + 1);
+        childIter.advance();
+    }
+}
+
+// ========= Implementation of XMLParser ============
 bool XMLParser::validateXML(const string& xmlDocument) {
     stack<string> tagStack;
     bool hasRoot = false;
@@ -47,6 +65,50 @@ bool XMLParser::validateXML(const string& xmlDocument) {
 
     return tagStack.empty() && hasRoot;
 }
+
+// ============== Built XML Tree =====================
+void XMLParser::builtTree(const string& xmlDocument) {
+    stack<Tree<string>*> nodeStack;
+
+    for (size_t i = 0; i < xmlDocument.size(); ++i) {
+        if (xmlDocument[i] == '<') {
+            size_t start = i + 1;
+            size_t end = xmlDocument.find('>', start);
+            if (end == string::npos) {
+                return;
+            }
+
+            string tag = xmlDocument.substr(start, end - start);
+            if (tag.empty()) {
+                return;
+            }
+
+            if (tag[0] != '/') {
+
+                Tree<string>* newNode = new Tree<string>(tag);
+                if (!nodeStack.empty()) {
+                    Tree<string>* parent = nodeStack.top();
+                    parent->children->append(newNode);
+                    newNode->parent = parent;
+                }
+                else {
+                    root = newNode;
+                }
+                nodeStack.push(newNode);
+            }
+            else {
+                nodeStack.pop();
+            }
+
+            i = end;
+        }
+    }
+}
+
+XMLParser::XMLParser(const string& xmlFileName) : xmlFileName(xmlFileName), root(nullptr) {}
+
+
+
 
 int main() {
     const std::string xmlFileName = "C:/Users/User/source/repos/ADS_2023_CA2_Erling_Munguia/ADS_2023_CA2_Erling_Munguia/Example.xml";
