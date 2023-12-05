b07db9527d6df8031cda6f010aade8979dccc9c6
Erlinguss <D00251914@student.dkit.ie>
2023-12-05 12:43:58 +0000

    Debugging the count function to pass the Failing TestMultipleLevels.

--- TreeTest/TestIterator.cpp
+++ TreeTest/TestIterator.cpp
@@ -1,4 +1,17 @@
 #include "pch.h"
-#include "TestIterator.h"
+//#include "TestIterator.h"
+#include "CppUnitTest.h"
+#include "../ADS_2023_CA2_Erling_Munguia/Tree.h"
 
 
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+namespace TestIterator
+{
+	TEST_CLASS(TestIterator)
+	{
+	public:
+
+	};
+}
--- TreeTest/TreeTest.cpp
+++ TreeTest/TreeTest.cpp
@@ -18,11 +18,13 @@ namespace TreeTest
 			Assert::IsNull(tree.parent);
 			Assert::AreEqual(0, tree.children.size());
 		}
+
 		TEST_METHOD(TestCountOneItem)
 		{
 			Tree<int> tree(1);
 			Assert::AreEqual(1, tree.count());
 		}
+
 		TEST_METHOD(TestCountOneItemWithChildrenAndSubChildren)
 		{
 			Tree<int> tree(1);
@@ -35,5 +37,36 @@ namespace TreeTest
 			Assert::AreEqual(2, childTwo.count());
 			Assert::AreEqual(4, tree.count());
 		}
+
+		TEST_METHOD(TestEmptyTree)
+		{
+			Tree<int> emptyTree(0);
+			Assert::AreEqual(1, emptyTree.count()); 
+			Assert::IsNull(emptyTree.parent);
+			Assert::AreEqual(0, emptyTree.children.size());
+		}
+
+		TEST_METHOD(TestMultipleLevels)
+		{
+			Tree<int> tree(1);
+			Tree<int> childOne(2);
+			Tree<int> childTwo(3);
+			Tree<int> subChild(4);
+			Tree<int> subSubChild(5);
+			subChild.children.append(&subSubChild);
+			childTwo.children.append(&subChild);
+			tree.children.append(&childOne);
+			tree.children.append(&childTwo);
+			Assert::AreEqual(4, tree.count()); 
+		}
+
+		TEST_METHOD(TestDataRetrieval)
+		{
+			Tree<std::string> stringTree("Root");
+			Assert::AreEqual(std::string("Root"), stringTree.getData());
+		}
+
+	
+
 	};
 }
