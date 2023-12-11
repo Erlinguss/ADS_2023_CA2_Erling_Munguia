2630874985563887e324c21fadef250f5ef15a51
Erlinguss <D00251914@student.dkit.ie>
2023-12-11 21:28:31 +0000

    Displaying Tree Structure in Window 😁

--- ADS_2023_CA2_Erling_Munguia/mainApp.cpp
+++ ADS_2023_CA2_Erling_Munguia/mainApp.cpp
@@ -15,8 +15,9 @@ const int ButtonHeight = 40;
 
 sf::RenderWindow window(sf::VideoMode(WindowWidth, WindowHeight), "Data Structure Management");
 
-void drawNode(sf::RenderWindow& window, const File* file, float x, float y) {
-    
+void drawNode(sf::RenderWindow& window, File* file, float x, float y) {
+
+
     sf::CircleShape circle(20);
     circle.setFillColor(sf::Color::White);
     circle.setOutlineColor(sf::Color::Black);
@@ -24,7 +25,7 @@ void drawNode(sf::RenderWindow& window, const File* file, float x, float y) {
     circle.setPosition(x, y);
     window.draw(circle);
 
-   
+
     sf::Font font;
     font.loadFromFile("C:/going-to-do-great-things/goingtodogreatthings.ttf");
     sf::Text text(file->name, font, 14);
@@ -34,26 +35,21 @@ void drawNode(sf::RenderWindow& window, const File* file, float x, float y) {
 
 void drawTreeRecursive(sf::RenderWindow& window, Tree<File*>* root, float x, float y, float xOffset) {
     if (root != nullptr) {
-      
         drawNode(window, root->getData(), x, y);
 
-        
         float childX = x - xOffset;
         float childY = y + 80;
 
         DListIterator<Tree<File*>*> childIter = root->children.getIterator();
         while (childIter.isValid()) {
-            
             sf::VertexArray line(sf::Lines, 2);
             line[0].position = sf::Vector2f(x + 20, y + 20);
             line[1].position = sf::Vector2f(childX + 20, childY + 20);
             line[0].color = sf::Color::Black;
             line[1].color = sf::Color::Black;
 
-            
             window.draw(line);
 
-           
             drawTreeRecursive(window, childIter.item(), childX, childY, xOffset / 2);
             childIter.advance();
             childX += xOffset;
@@ -61,14 +57,49 @@ void drawTreeRecursive(sf::RenderWindow& window, Tree<File*>* root, float x, flo
     }
 }
 
+sf::FloatRect calculateTreeBounds(Tree<File*>* root, float x, float y, float xOffset) {
+    sf::FloatRect bounds(x, y, 40, 40);
+
+    if (root != nullptr) {
+        float childX = x - xOffset;
+        float childY = y + 80;
+
+        DListIterator<Tree<File*>*> childIter = root->children.getIterator();
+        while (childIter.isValid()) {
+            sf::FloatRect childBounds = calculateTreeBounds(childIter.item(), childX, childY, xOffset / 2);
+            bounds.left = std::min(bounds.left, childBounds.left);
+            bounds.top = std::min(bounds.top, childBounds.top);
+            bounds.width = std::max(bounds.width, childBounds.left + childBounds.width);
+            bounds.height = std::max(bounds.height, childBounds.top + childBounds.height);
+
+            childIter.advance();
+            childX += xOffset;
+        }
+    }
+
+    return bounds;
+}
+
+
+
 void drawTree(sf::RenderWindow& window, const XMLParser& xmlParser) {
-    
     if (xmlParser.getRoot() != nullptr) {
-       
-        drawTreeRecursive(window, xmlParser.getRoot(), WindowWidth / 2, 50, WindowWidth / 4);
+        float centerX = static_cast<float>(window.getSize().x) / 2.0f;
+        float centerY = static_cast<float>(window.getSize().y) / 2.0f;
+
+        sf::FloatRect treeBounds = calculateTreeBounds(xmlParser.getRoot(), centerX, centerY, WindowWidth / 4);
+
+        float minWidth = 200.0f;
+        float minHeight = 200.0f;
+
+        sf::View view(sf::FloatRect(0, 0, std::max(treeBounds.width + 20, minWidth), std::max(treeBounds.height + 20, minHeight)));
+        view.setCenter(centerX, centerY);
+
+        window.setView(view);
+
+        drawTreeRecursive(window, xmlParser.getRoot(), centerX, centerY, WindowWidth / 4);
     }
     else {
-       
         sf::Font font;
         font.loadFromFile("C:/going-to-do-great-things/goingtodogreatthings.ttf");
 
@@ -79,6 +110,7 @@ void drawTree(sf::RenderWindow& window, const XMLParser& xmlParser) {
         window.draw(errorText);
     }
 }
+
 
 void drawGUI() {
     window.clear(sf::Color::White);
@@ -99,10 +131,7 @@ void drawGUI() {
     button2.setFillColor(sf::Color::Blue);
     button2.setPosition(10, 60);
     window.draw(button2);
-
-    // window.display();
 }
-
 
 bool handleEvent(XMLParser& xmlParser) {
     sf::Event event;
@@ -118,8 +147,6 @@ bool handleEvent(XMLParser& xmlParser) {
 
                 sf::FloatRect buttonBounds1(10, 10, ButtonWidth, ButtonHeight);
                 sf::FloatRect buttonBounds2(10, 60, ButtonWidth, ButtonHeight);
-                sf::FloatRect buttonBounds3(10, 90, ButtonWidth, ButtonHeight);
-                sf::FloatRect buttonBounds4(10, 120, ButtonWidth, ButtonHeight);
 
                 if (buttonBounds1.contains(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y))) {
                     std::cout << "Button 1 Clicked!\n" << std::endl;
@@ -147,7 +174,6 @@ bool handleEvent(XMLParser& xmlParser) {
     return true;
 }
 
-
 int main() {
     std::string xmlFileName;
     XMLParser xmlParser("");
@@ -158,7 +184,6 @@ int main() {
         drawTree(window, xmlParser);
         window.display();
     }
-
 
     return 0;
 }
